0ae3eb31a9ad35b1ac3f821d5f363f53dbe0028d
Mauricio Muratori <maurimura@gmail.com>
2019-10-31 16:51:17 -0300

    Reciving messages from client

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use actix::{Actor, StreamHandler};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web_actors::ws;
+use listenfd::ListenFd;
+
+/// Define http actor
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<ws::Message, ws::ProtocolError> for MyWs {
+    fn handle(&mut self, msg: ws::Message, ctx: &mut Self::Context) {
+        println!("WS: {:?}", msg);
+        match msg {
+            ws::Message::Ping(msg) => ctx.pong(&msg),
+            ws::Message::Text(text) => {
+                let addr = ctx.address();
+                println!("{:?}", addr)
+                // ctx.text(text)
+            },
+            ws::Message::Binary(bin) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    resp
+}
+
+fn main() {
+    // For dev only
+    let mut listenfd = ListenFd::from_env();
+
+    let mut server = HttpServer::new(|| App::new().route("/ws/", web::get().to(index)));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l).unwrap()
+    } else {
+        server.bind("127.0.0.1:3000").unwrap()
+    };
+
+    server.run().unwrap();
+    // .bind("127.0.0.1:8088")
+    //     .unwrap()
+    //     .run()
+    //     .unwrap();
+}
