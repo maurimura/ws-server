e8ced337d85f7ef8c433718a8c526fbaecb30549
Mauricio Muratori <maurimura@gmail.com>
2019-12-29 19:24:30 -0300

    Actix_web 2.0 installed. Rewrite started

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,21 @@
-mod node;
-mod server;
-extern crate json;
+use actix_web::{web, App, HttpResponse, HttpServer, HttpRequest, Error};
+use actix_web_actors::ws;
 
-use actix::*;
-use actix_files as fs;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use std::collections::{HashMap};
+mod handler;
 
+use handler::MyWs;
 
-use server::*;
-
-fn main() -> std::io::Result<()> {
-    env_logger::init();
-    let sys = System::new("ws-server");
-
-    // Start chat server actor
-    let ws_server = Server {
-        name: "Server".to_string(),
-        clients: HashMap::new(),
-    };
-    let server = ws_server.start();
-
-    HttpServer::new(move || {
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
         App::new()
-            .data(server.clone())
-            .service(web::resource("/").to(|| {
-                HttpResponse::Found()
-                    .header("LOCATION", "/static/index.html")
-                    .finish()
-            }))
-            .service(fs::Files::new("/static/", "client/"))
-            .service(web::resource("/ws/{name}").to(index))
-            .service(web::resource("/ws/").to(index))
+            .route("/", web::get().to(index))
     })
-    .bind("127.0.0.1:3000")?
-    .start();
-
-    sys.run()
-
-
+    .bind("127.0.0.1:8088")?
+    .run()
+    .await
 }
